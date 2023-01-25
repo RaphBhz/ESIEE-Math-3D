7b944f0af10c4d5279fb16b15918c68ed5432ecf
RaphBhz <raphael.bouchez@edu.esiee.fr>
2023-01-25 18:09:20 +0100

    feature: gravity

---
+++ E3FI_2022_2023_Maths3D_Raylib_BaseProject_VS2019/projects/VS2019/examples/Physics.h
@@ -0,0 +1,35 @@
+#include <raymath.h>
+#include "My3DPrimitives.h"
+#pragma once
+float g = 9.80665;
+Vector3 G = { 0, -g, 0 };
+
+struct Physics {
+	float mass;
+	float energy;
+	float speed;
+
+	Vector3 velocity;
+};
+
+Physics InitPhysics(Sphere object, Vector3 initialVelocity, float speed, float mass)
+{
+	Physics phys;
+	phys.mass = mass;
+	phys.speed = speed;
+	phys.velocity = initialVelocity;
+	phys.energy = mass/2 * powf(speed, 2) + mass * g * object.ref.origin.y;
+	return phys;
+}
+
+void GetNewVelocity(float y, float ellapsedTime, Physics& phys)
+{
+	phys.velocity = Vector3Normalize(Vector3Add(phys.velocity, Vector3Scale(G, ellapsedTime)));
+}
+
+void GetSphereGravitationalTranslation(float y, float ellapsedTime, Physics& phys)
+{
+	GetNewVelocity(y, ellapsedTime, phys);
+	float a = (phys.energy - phys.mass * g * y);
+	phys.speed = sqrtf(2 * ((phys.energy - phys.mass * g * y) / phys.mass));
+}
